95a47d7d1c3bd335878b408a8c13f232b216b46b
felixNyalenda <felixasejunior@gmail.com>
2023-06-22 10:07:51 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+puts "🌱 Seeding spices..."
+
 author1 = Author.create(name: "Leeroy Jenkins")
 author2 = Author.create(name: Faker::Name.unique.name)
 author3 = Author.create(name: Faker::Name.unique.name)
 
-Profile.create(author_id: author1.id, username: "ljenk", email: "ljenk@aol.com", bio: "a very dated reference")
+Profile.create(author_id: author1.id, username: "ljenk", email: "ljenk@aol.com", bio: "a very dated reference", avatar_url:"https://images.pexels.com/photos/15964776/pexels-photo-15964776/free-photo-of-brunette-in-white-pants.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load", facebook: 'https://facebook.com')
+Profile.create(author_id: author2.id, username: "f.olali", email: "folali@mail.com", bio: "a very dedicated web developer", avatar_url:"https://images.pexels.com/photos/16977812/pexels-photo-16977812/free-photo-of-fashion-man-people-dark.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load", facebook: 'https://twiiter.com')
 
 post1 = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, author_id: author1.id)
 post2 = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, author_id: author2.id)
@@ -23,3 +26,5 @@ PostTag.create(post_id: post3.id, tag_id: tag2.id)
 
 PostTag.create(post_id: post4.id, tag_id: tag2.id)
 PostTag.create(post_id: post4.id, tag_id: tag3.id)
+
+puts "✅ Done seeding!"
